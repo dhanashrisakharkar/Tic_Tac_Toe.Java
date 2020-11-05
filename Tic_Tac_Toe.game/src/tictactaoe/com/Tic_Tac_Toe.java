6533ec08a1057ecc310726b83875a776e700bb42
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-05 12:06:29 +0530

    Added : Select From Option X or O

--- Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
+++ Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
@@ -10,31 +10,30 @@ public class Tic_Tac_Toe {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
 		EmptyBorad();
+		player = selectoption();
+		System.out.println("Selected option by player is "+player);
 	}
 
 	static void EmptyBorad() {
 		for (int a = 1; a < board.length; a++) {
 			board[a] = ' ';
-			player = selectoption();
-			Computer = player == 'X' ? 'O' : 'X';
-			System.out.println(player +" Comte is "+Computer);
+			
 		}
 
 	}
 
 	static char selectoption() {
 		while (true) {
-			System.out.println("Select the option X or 0");
+			System.out.println("Select the option X or O");
 			player = sc.next().charAt(0);
-			if (player == 'X' || player == '0') {
+			if (player == 'X' || player == 'O') {
 				return player;
 			} else {
 				System.out.println("invalid choice enter again");
-				player = '-';
-				break;
+				System.out.println("---------");
+				continue;
 			}
 		}
-		return player;
 	}
 
 }
