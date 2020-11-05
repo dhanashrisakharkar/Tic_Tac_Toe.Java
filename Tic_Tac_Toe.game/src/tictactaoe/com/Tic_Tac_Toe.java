bb4c71a229121bc97f0094430fc3fb9a46d07c74
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-05 10:44:37 +0530

    Added : Create Empty Board With Index

--- Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
+++ Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
@@ -1,7 +1,19 @@
 package tictactaoe.com;
 
 public class Tic_Tac_Toe {
+	static char[] board = new char[10];
+
 	public static void main(String[] args) {
-		System.out.println("Welome to Tic Tac Toe Game");
+		System.out.println("Welcome to Tic Tac Toe Game");
+
+		EmptyBorad();
+
+	}
+
+	    static void EmptyBorad() {
+		for (int a = 1; a < 10; a++) {
+			board[a] = ' ';
+		}
+
 	}
 }
