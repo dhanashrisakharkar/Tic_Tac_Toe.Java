15bdb1333e92edca7336cb79c1f14d9153bf53f5
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-05 10:41:12 +0530

    Added : Welcome To Tic Tac Toe Game

--- Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
+++ Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
@@ -1,20 +1,7 @@
 package tictactaoe.com;
 
 public class Tic_Tac_Toe {
-	static char[] board = new char[10];
-
 	public static void main(String[] args) {
-		
-		EmptyBorad();
-	
-
-	}
-
-	private static void EmptyBorad() {
-		for(int a=1;a<10;a++)
-		{
-			board[a]= ' ';
-		}
-		
+		System.out.println("Welome to Tic Tac Toe Game");
 	}
 }
