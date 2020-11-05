5f207ff3336e34e2dd851006388a55bc94b9fd1e
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-11-05 13:20:43 +0530

    Added : Display The Current Board

--- Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
+++ Tic_Tac_Toe.game/src/tictactaoe/com/Tic_Tac_Toe.java
@@ -3,27 +3,27 @@ package tictactaoe.com;
 import java.util.Scanner;
 
 public class Tic_Tac_Toe {
+	static int index;
 	static char[] board = new char[10];
-	static char player, Computer;
+	static char player, computer;
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Game");
-		EmptyBorad();
-		player = selectoption();
-		Computer = player == 'X' ? 'O' : 'X';
-		System.out.println(player + " is player " + Computer + " is computer");
+		CreateBorad();
+		player = selectOption();
+		computer = player == 'X' ? 'O' : 'X';
+		System.out.println(player + " is player " + computer + " is computer");
+		showBoard();
 	}
 
-	static void EmptyBorad() {
-		for (int index = 1; index < board.length; index++) {
+	static void CreateBorad() {
+		for (index = 1; index < board.length; index++) {
 			board[index] = ' ';
-
 		}
-
 	}
 
-	static char selectoption() {
+	static char selectOption() {
 		while (true) {
 			System.out.println("Select the option X or O");
 			player = sc.next().charAt(0);
@@ -37,4 +37,9 @@ public class Tic_Tac_Toe {
 		}
 	}
 
+	static void showBoard() {
+		for (index = 1; index < board.length; index = index + 3) {
+			System.out.println("[" + board[index] + "|" + board[index + 1] + "|" + board[index + 2] + "]");
+		}
+	}
 }
